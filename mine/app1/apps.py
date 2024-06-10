3ecd6e5142ee7c9d3b07c733ff9d72e5517fa576
endaevor <101499715+ndaevor@users.noreply.github.com>
2024-06-10 12:04:13 +0530

    Delete mine directory

--- mine/app1/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-#my app
-class App1Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app1'
--- mine/app1/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Document
-
-
-#form
-class DocumentForm(forms.ModelForm):
-    class Meta:
-        model = Document
-        fields = ['file']
